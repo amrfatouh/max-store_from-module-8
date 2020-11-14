b7e36cff09ba7e87aa11a160e6eada1070499b1d
amrfatouh <66204477+amrfatouh@users.noreply.github.com>
2020-11-15 00:58:19 +0200

    Cart model - posting request to cart - adding items to cart'

---
+++ models/Cart.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const path = require("path");
+
+const Product = require("./product");
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "cart.json"
+);
+
+module.exports = class Cart {
+  static addToCart(id) {
+    // fetching the cart
+    fs.readFile(p, (err, fileContent) => {
+      let cart = { products: [], totalPrice: 0 };
+      if (!err) cart = JSON.parse(fileContent);
+      Product.getProductById(id, (product) => {
+        let existingProduct = cart.products.find((prod) => prod.id === id);
+        // check if the product already exists in the cart or not
+        if (existingProduct) {
+          // the product exists => increase the quantity
+          existingProduct.quantity += 1;
+        } else {
+          // the product doesn't exist => add a new product
+          cart.products.push({ id: id, quantity: 1 });
+        }
+        // increase the total price
+        cart.totalPrice += Number(product.price);
+
+        // rewrite the file to save the new cart
+        fs.writeFile(p, JSON.stringify(cart), (err) => console.log(err));
+      });
+    });
+  }
+};
